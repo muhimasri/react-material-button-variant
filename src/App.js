ee980d772a6561523a03c4d5298d8c4a6afe150a
Muhi Masri <muhi@muhimasri.com>
2021-04-20 07:45:38 -0400

    Test button

--- src/App.js
+++ src/App.js
@@ -4,9 +4,8 @@ import { Button } from './Button';
 function App() {
   return (
     <div className="App">
-      <Button variant="text" color="primary">Text Button!</Button>
-      <Button variant="contained" color="primary">Contained Button!</Button>
-      <Button variant="outlined" color="primary">Outlined Button!</Button>
+      <Button variant="contained" color="primary" pill>Contained Pill!</Button>
+      <Button variant="outlined" color="primary" pill>Outlined Pill!</Button>
     </div>
   );
 }
